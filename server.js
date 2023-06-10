c85267b7800a9f256a0faae13642f9e31d90b5f6
annisabd <annisabd@ais.co.th>
2023-06-10 13:40:01 +0700

    express.static

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const appPath = path.join(__dirname, 'dist', 'annidev-client');
 const port = process.env.PORT || 8080;
 
-app.use(express.static(appPath));
-
-app.all('*', (req, res) => {
-  res.status(200).sendFile('/', { root: appPath });
+app.use(express.static(__dirname + '/dist/annidev-client'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/annidev-client/index.html'));
 });
 
 app.listen(port, () => {

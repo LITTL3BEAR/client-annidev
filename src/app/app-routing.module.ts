5f58c01a7aa9948b23aaf2d5474ed28a6921e309
annisabd <annisabd@ais.co.th>
2024-01-03 00:27:27 +0700

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/manga/manga.module').then(m => m.MangaModule) },
   { path: 'manga', loadChildren: () => import('./modules/manga/manga.module').then(m => m.MangaModule) },
   { path: 'store', loadChildren: () => import('./modules/store/store.module').then(m => m.StoreModule) }
 ];

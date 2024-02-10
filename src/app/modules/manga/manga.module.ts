5a061dad6835b77ec9c5110371d30e6d9cfb5503
annisabd <annisabd@ais.co.th>
2024-02-10 18:50:20 +0700

    -

--- src/app/modules/manga/manga.module.ts
+++ src/app/modules/manga/manga.module.ts
@@ -5,15 +5,18 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule } from '@angular/material/dialog';
+
 import { MangaRoutingModule } from './manga-routing.module';
 import { MangaComponent } from './manga.component';
 import { MangaFormComponent } from './manga-form/manga-form.component';
 import { MangaListComponent } from './manga-list/manga-list.component';
 import { MangaService } from './manga.service';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -29,10 +32,12 @@ import { MangaService } from './manga.service';
     MatPaginatorModule,
     ReactiveFormsModule,
     MatInputModule,
+    MatSelectModule,
     MatButtonModule,
     MatIconModule,
     MatTooltipModule,
-    MatDialogModule
+    MatDialogModule,
+    SharedModule,
   ],
   providers: [MangaService],
 })
